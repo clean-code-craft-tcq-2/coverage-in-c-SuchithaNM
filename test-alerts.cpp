080986111ad4846320d0bb6b5ce50fb16ba84c11
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-20 14:06:20 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -28,13 +28,13 @@ TEST_CASE("Classify temperature Breach as a Mid active cooling  ") {
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify  too High Tempature  ") {
-	BatteryCharacter batteryChar = {HI_ACTIVE_COOLING,"BMS"};
+	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
   REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,60) == SEND_TO_EMAIL_TOO_HIGH);
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify  Noraml Tempature   ") {
-	BatteryCharacter batteryChar = {MED_ACTIVE_COOLING,"BMS"};
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,15) == SEND_TO_EMAIL_NORMAL);
+	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,20) == SEND_TO_EMAIL_NORMAL);
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify too Low Tempearture  ") {
