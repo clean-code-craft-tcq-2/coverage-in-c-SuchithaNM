c059752ba36bc86f8f1c4f2c16a1d6468676a609
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-20 22:40:41 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -35,15 +35,15 @@ TEST_CASE("Check and Send Alert notification to controller ") {
 
 TEST_CASE("Check and Send Email Alert to receipt to notify Tempature Breach Passive cooling") {
 	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
-//  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,20) == SEND_TO_EMAIL_NORMAL);
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,-2 ) == SEND_TO_EMAIL_TOO_LOW);
+//  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,30) == SEND_TO_EMAIL_NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,0 ) == SEND_TO_EMAIL_TOO_LOW);
 //  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,45) == SEND_TO_EMAIL_TOO_HIGH);
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify too Low Tempearture  ") {
 	BatteryCharacter batteryChar = {MED_ACTIVE_COOLING,"BMS"};
 //  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,25) == SEND_TO_EMAIL_NORMAL);
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,-3 ) == SEND_TO_EMAIL_TOO_LOW);
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,0 ) == SEND_TO_EMAIL_TOO_LOW);
 //  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,55) == SEND_TO_EMAIL_TOO_HIGH);
 }
 
