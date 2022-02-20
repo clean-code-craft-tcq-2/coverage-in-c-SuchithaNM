98571263b17f98f5f6a708feb0574e196450aefa
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-02-20 19:35:18 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,51 +3,7 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits for Low case") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
 
-TEST_CASE("infers the breach according to limits for high case") {
-  REQUIRE(inferBreach(30, 0, 20) == TOO_HIGH);
-}
-
-TEST_CASE("infers the breach according to limits for normal case") {
-  REQUIRE(inferBreach(12, 10, 20) == NORMAL);
-}
-
-TEST_CASE("infers the breach according to limits in HI_ACTIVE_COOLING for NORMAL case") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL);
-}
-
-TEST_CASE("infers the breach according to limits in PASSIVE_COOLING for TOO_LOW case") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -4) == TOO_LOW);
-}
-
-TEST_CASE("infers the breach according to limits in MED_ACTIVE_COOLING for TOO_HIGH case") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 60) == TOO_HIGH);
-}
-
-TEST_CASE("Check and alert functionality check to controller in case of PASSIVE_COOLING:NORMAL") {
-	 BatteryCharacter batterycharname= {PASSIVE_COOLING, "BMS"};
-    REQUIRE( checkAndAlert(TO_CONTROLLER, batterycharname, 30)== SEND_TO_CONTROLLER);
-}
-
-TEST_CASE("Check and alert functionality check to email in case of PASSIVE_COOLING:NORMAL") {
-	 BatteryCharacter batterycharname= {PASSIVE_COOLING, "BMS"};
-     REQUIRE(checkAndAlert(TO_EMAIL, batterycharname, 30)==SEND_TO_EMAIL_NORMAL);
-}
-
-TEST_CASE("Check and alert functionality check to email in case of PASSIVE_COOLING:TOO_LOW") {
-	 BatteryCharacter batterycharname= {PASSIVE_COOLING, "BMS"};
-     REQUIRE(checkAndAlert(TO_EMAIL, batterycharname, -4)==SEND_TO_EMAIL_TOO_LOW);
-}
-
-TEST_CASE("Check and alert functionality check to email in case of PASSIVE_COOLING:TOO_HIGH") {
-	 BatteryCharacter batterycharname= {PASSIVE_COOLING, "BMS"};
-     REQUIRE(checkAndAlert(TO_EMAIL, batterycharname, 50)==SEND_TO_EMAIL_TOO_HIGH);
-}
-
-/*
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
@@ -73,13 +29,13 @@ TEST_CASE("Classify temperature Breach as a Mid active cooling  ") {
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify  too High Tempature  ") {
-	BatteryCharacter batteryChar = {HI_ACTIVE_COOLING,"BMS"};
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,120) == SEND_TO_EMAIL_TOO_HIGH);
+	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,45) == SEND_TO_EMAIL_TOO_HIGH);
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify  Noraml Tempature   ") {
 	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,40) == SEND_TO_EMAIL_NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,20) == SEND_TO_EMAIL_NORMAL);
 }
 
 TEST_CASE("Check and Send Email Alert to receipt to notify too Low Tempearture  ") {
@@ -90,5 +46,4 @@ TEST_CASE("Check and Send Email Alert to receipt to notify too Low Tempearture  
 TEST_CASE("Check and Send Alert notification to controller ") {
 	BatteryCharacter batteryChar = {PASSIVE_COOLING,"BMS"};
   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,30 ) == SEND_TO_CONTROLLER);
-}*/
-  
+}
